ea95f7783c37246d9110d230bfd759925f63551a
jv <jv@acme.com>
2016-01-30 21:28:15 -0600

    Initial commit

---
+++ DemoApp/DemoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Customer cust = new Customer
+            {
+                ID = 1,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+        }
+    }
+}
